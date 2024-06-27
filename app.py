c9b4ecaa5eb600537877f993d5835b9f39478688
AlexanderGoose <alexandergoose97@gmail.com>
2024-06-27 16:42:11 -0600

    added navbar and general styling for it

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    pass
+
+@app.route('/resume')
+def resume():
+    pass
