a35a5c9ac810aa7983adf8ba029dcea2191c5805
AlexanderGoose <alexandergoose97@gmail.com>
2024-11-09 19:15:52 -0700

    fixing env issue

--- weather.py
+++ weather.py
@@ -10,8 +10,9 @@ import os
 import pytz
 
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?'
-API_KEY = open('api_key.txt', 'r').read().strip()
-# API_KEY = os.getenv('API_KEY')
+# API_KEY = open('api_key.txt', 'r').read().strip()
+
+API_KEY = os.getenv('API_KEY')
 CITY = 'Lakewood'
 
 def kelvin_to_far(kelvin):
